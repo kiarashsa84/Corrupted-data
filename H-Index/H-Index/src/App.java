cbd058b317046bd3cd0849dd87a992f442338e7f
kiarashsa84 <72kiarash84@gmail.com>
2024-03-11 16:02:04 +0330

    I've done 90% of h-index project and I get a runtime error

--- H-Index/H-Index/src/App.java
+++ H-Index/H-Index/src/App.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); 
+
+        int count_article = input.nextInt(); 
+
+        int[] citation = new int[count_article]; 
+
+        for (int i = 0 ; i < count_article; i++){
+            citation[i] = input.nextInt(); 
+        }
+
+        for (int i = 0; i < citation.length; i++){
+            for (int j = i +1 ; j < citation.length; j++){
+                if (citation[i] > citation[j]){
+                    int tmp = citation[j]; 
+                    citation[j] = citation[i]; 
+                    citation[i] = tmp; 
+                }
+            }
+        } 
+
+        int h_index = citation.length ; 
+        int cnt =0; 
+
+        while (true){
+            if (citation[cnt] >= h_index){
+                break; 
+            }
+            else{
+                h_index--; 
+                citation[cnt++] = -1;
+            }
+        }
+        System.out.println(h_index);
+
+        
     }
 }
