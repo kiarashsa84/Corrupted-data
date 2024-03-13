32c256e4e5cf49533f863bfe9109d19d1c5897dd
kiarashsa84 <72kiarash84@gmail.com>
2024-03-13 13:37:41 +0330

    I finished XSS project but I gte 82%

--- XSS/XSS/src/App.java
+++ XSS/XSS/src/App.java
@@ -1,11 +1,14 @@
 import java.util.Scanner; 
 
 public class App {
+
+
+
     public static void main(String[] args) throws Exception {
         
         Scanner input  = new Scanner(System.in); 
 
-        String[] usernames = new String[100]; 
+        String[][] usernames = new String[100][2]; 
 
         boolean exist = false; 
 
@@ -16,15 +19,32 @@ public class App {
         while(!exit){
 
             String menu = input.nextLine(); 
+             
+
+            if (checkxss(menu) || checkxss_2(menu)){
+
+                System.out.println("XSS Attack!");
+                continue; 
+
+            }
 
             
             switch (menu) {
 
                 case "signup":
-
+                // System.out.println("signup mode");
+                
+                
+                // System.out.println("enter your username: ");
                 String username = input.nextLine(); 
+                if (checkxss(username) || checkxss_2(username)){
+
+                    System.out.println("XSS Attack!");
+                    continue; 
+    
+                }
                     for (int i = 0 ; i < 100; i++){
-                        if(username == usernames[i]) {
+                        if(username.equals(usernames[0])) {
                             exist = true; 
                             break;
                         } 
@@ -36,36 +56,107 @@ public class App {
                     }
 
                     else {
-                        usernames[cout_users] = username; 
-                        cout_users++; 
-                    }
-
-                    String password = input.nextLine(); 
-                    boolean verify_cap = true;
-                    boolean verify_digit = true;
-                    boolean symbols = true;
-
-
-                    if (password.length() < 8){
-
-                        System.out.println("Password Not Valid!");
-                        break;
-
-                    }else{
-                        System.out.println("complete this part");
-                    }
-
-
-
-
+                        
                     
+                        // System.out.println("enter your password: ");
+                        String password = input.nextLine(); 
+
+                        if (checkxss(password) || checkxss_2(password)){
+
+                            System.out.println("XSS Attack!");
+                            continue; 
+            
+                        }
+                        boolean verify_cap = false;
+                        boolean lower = false; 
+                        boolean verify_digit = false;
+                        boolean symbols = false;
+
+
+                        if (password.length() < 8){
+
+                            System.out.println("Password Not Valid!");
+                            
+
+                        }else{
+                            for (char c: password.toCharArray()){
+                                if(Character.isDigit(c)){
+                                    verify_digit = true; 
+                                }
+                                else if(Character.isUpperCase(c)){
+                                    verify_cap = true;
+                                }
+                                else if(Character.isLowerCase(c)){
+                                    lower = true; 
+                                }else if(!Character.isLetterOrDigit(c)){
+                                    symbols = true;
+                                }
+                            }
+
+
+                            if (verify_cap & lower & verify_digit & symbols){
+                            
+                                usernames[cout_users][0] = username;
+                                usernames[cout_users][1] = password;  
+                                cout_users++; 
+                                System.out.println("SignUp Successful!");
+                            
+                            } else{
+                                System.out.println("Password Not Valid!");
+
+                            }
+                        
+                    }
+
+                }
 
                     break;
 
                 case "login":
+                    // System.out.println("hello");
+
+                    String inUser = input.nextLine();
+                    
+                    
+                    if (checkxss(inUser) || checkxss_2(inUser)){
+
+                        System.out.println("XSS Attack!");
+                        continue; 
+        
+                    }
+
+
+                    String inPass = input.nextLine(); 
+
+
+                    
+                    boolean suclogin = false; 
+
+                    for(int i = 0; i < 100; i++){
+
+                        if(inUser.equals(usernames[i][0])){
+                            if(inPass.equals(usernames[i][1])){
+                                System.out.println("Login Successful!");
+                                suclogin = true;
+
+                            }
+                        }
+                        
+
+                    }
+
+                    if(!suclogin){
+                        System.out.println("Invalid Username or Password!");
+                    }
+
+
+
+
                     break; 
                 
                 case "exit":
+
+                    exit = true;
                     break; 
             
                 default:
@@ -73,4 +164,135 @@ public class App {
             }
         }
     }
+
+    public static boolean checkxss(String input){
+
+        String line = input.toLowerCase(); 
+
+        if (line.contains("<")){
+
+            if(line.contains("<script>")|| line.contains("</script>")){
+                return true;
+            }
+            else{ 
+
+                int indexoff = line.indexOf("<"); 
+                boolean space = true;
+                
+                if(line.charAt(++indexoff) ==  ' ' ){
+                    
+                    while(space){
+                        indexoff++; 
+                        if (line.charAt(indexoff) != ' '){
+                            // System.out.println(line.charAt(indexoff));
+                            space = false; 
+                        }
+
+                    }
+
+                    if (line.substring(indexoff, indexoff+6).equals("script")){
+                        indexoff += 6 ;
+                        space = true;
+                        //
+                        if (indexoff >= line.length()){
+                            return false; 
+                        }
+                        if (line.charAt(indexoff) == ' '){
+                            while(space){
+                                indexoff++; 
+                                if (line.charAt(indexoff) != ' '){
+                                    // System.out.println(line.charAt(indexoff));
+                                    space = false; 
+                                }
+        
+                            }        
+                            }
+                            
+                        }
+                        if(line.charAt(indexoff) == '>'){
+                            return true;
+                        }
+                        
+                    }
+                }   
+
+            }
+
+            return false; 
+    }
+
+
+
+
+
+    public static boolean checkxss_2(String input){
+
+        String line = input.toLowerCase(); 
+
+        if (line.contains("<")){
+
+            if(line.contains("<script>")|| line.contains("</script>")){
+                return true;
+            }
+            else{ 
+
+                int indexoff = line.indexOf("<"); 
+                boolean space = true;
+                
+                if(line.charAt(++indexoff) ==  ' ' ){
+                    
+                    while(space){
+                        indexoff++; 
+                        if (line.charAt(indexoff) != ' '){
+                            // System.out.println(line.charAt(indexoff));
+                            space = false; 
+                        }
+
+                    }
+
+                    if (line.charAt(indexoff) == '/'){
+                        indexoff += 1 ;
+                        space = true;
+                        if (line.charAt(indexoff) == ' '){
+                            while(space){
+                                indexoff++; 
+                                if (line.charAt(indexoff) != ' '){
+                                    // System.out.println(line.charAt(indexoff));
+                                    space = false; 
+                                }
+        
+                            }        
+                            }
+                            
+                        }
+                        if(line.substring(indexoff, indexoff+6).equals("script")){
+                            indexoff += 6 ;
+                            if(line.charAt(indexoff) ==  ' ' ){
+                    
+                                while(space){
+                                    indexoff++; 
+                                    if (line.charAt(indexoff) != ' '){
+                                        // System.out.println(line.charAt(indexoff));
+                                        space = false; 
+                                    }
+            
+                                }
+
+                                
+                        }
+                        if(line.charAt(indexoff) == '>') {
+                            return true;
+                        }
+                    }
+                }   
+
+            }
+
+            
+    }
+    return false; 
 }
+
+
+}
+
